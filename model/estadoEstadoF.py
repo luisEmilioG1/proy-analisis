be5341b27a208813c83a4ddb6f196b8c7e6be561
luisEmilioG1 <luis.1702011450@ucaldas.edu.co>
2024-04-14 20:18:17 -0500

    generate EstadoEstadoF table with individual EstadoCanalF

--- model/estadoEstadoF.py
+++ model/estadoEstadoF.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 class EstadoEstadoF:
-    def __init__(self, array: np.ndarray):
-        self.array = array
+    def __init__(self, array: list):
+        self.array = np.array(array)
 
     def get_probability_distribution(self, index_combo: int):
         pass
